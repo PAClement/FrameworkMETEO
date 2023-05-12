43f505c31c2b3fe05333d61c322a2a423dccfbfc
PAClement <clementpaquentin@hotmail.fr>
2023-05-12 15:47:00 +0200

    Update

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,19 +5,6 @@ class AuthController {
       this.is_authenticate = false
     }
 
-    authentWhithGithub(req, res){
-      const params = queryString.stringify({
-        client_id: process.env.APP_ID,
-        redirect_uri: 'http://localhost:3000/auth',
-        scope: ['read:user', 'user:email'].join(' '), // space seperated string
-        allow_signup: true,
-      });
-      
-      const githubLoginUrl = `https://github.com/login/oauth/authorize?${params}`;
-      res.render(githubLoginUrl)
-      // a compléter
-    }
-
     isAuthenticate(){
         this.isAuthenticate = true;
     }
